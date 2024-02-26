785b416246de783b8bb4e3a5f2994f64d35b4491
jekyllpark <jekyllpark0327@gmail.com>
2024-02-26 16:51:44 +0900

    chore: 어플리케이션 네이밍 수정(boldpaws -> boldPaws)

--- src/main/java/com/dangdang/boldpaws/BoldPawsApplication.java
+++ src/main/java/com/dangdang/boldpaws/BoldPawsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoldpawsApplication {
+public class BoldPawsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoldpawsApplication.class, args);
+        SpringApplication.run(BoldPawsApplication.class, args);
     }
 
 }
--- src/test/java/com/dangdang/boldpaws/BoldPawsApplicationTests.java
+++ src/test/java/com/dangdang/boldpaws/BoldPawsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BoldpawsApplicationTests {
+class BoldPawsApplicationTests {
 
     @Test
     void contextLoads() {
