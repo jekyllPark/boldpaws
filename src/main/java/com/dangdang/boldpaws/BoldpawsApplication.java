93ac1113e31f3aab31eefe0b0e0dd283dd325cbb
jekyllpark <jekyllpark0327@gmail.com>
2024-02-26 14:29:58 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/dangdang/boldpaws/BoldpawsApplication.java
@@ -0,0 +1,13 @@
+package com.dangdang.boldpaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoldpawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoldpawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dangdang/boldpaws/BoldpawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangdang.boldpaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoldpawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
