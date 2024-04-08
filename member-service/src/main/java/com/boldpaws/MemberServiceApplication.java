fdd0535f8e47ec6f121c5ee39617edd50d236caa
jekyllpark <jekyllpark0327@gmail.com>
2024-04-09 07:57:08 +0900

    chore - 각 서비스 별 도커파일 생성 및 도커 컴포즈 통합 - 모듈 별 깃 이그노어 원복 rename - 모놀리스에서 모듈로 전환함으로써 어플리케이션 네이밍 변경

--- member-service/src/main/java/com/boldpaws/MemberServiceApplication.java
+++ member-service/src/main/java/com/boldpaws/MemberServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoldPawsApplication {
+public class MemberServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoldPawsApplication.class, args);
+        SpringApplication.run(MemberServiceApplication.class, args);
     }
 
 }
